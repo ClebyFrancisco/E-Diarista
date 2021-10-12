4baea96001f8be2d429439de1076bbe1d3b555d3
Cleby Francisco <clebysilva5@gmail.com>
2021-10-11 23:39:32 -0300

    User Information

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,27 @@
-import type { NextPage } from 'next';
+import type { NextPage } from "next";
 
-import PageTitle from 'ui/components/data-display/PageTitle/PageTitle';
-import SafeEnvironment from 'ui/components/feedback/SafeEnvironment/SafeEnvironment';
-
+import PageTitle from "ui/components/data-display/PageTitle/PageTitle";
+import UserInformation from "ui/components/data-display/UserInfomation/UserInformation";
+import SafeEnvironment from "ui/components/feedback/SafeEnvironment/SafeEnvironment";
 
 const Home: NextPage = () => {
   return (
     <div>
       <SafeEnvironment />
-      <PageTitle 
-      title={"Conheça Os profissionais"}
-      subtitle={'Preencha seu endereço e veja todos os profissionais da sua localidade'}/>
-     
+      <PageTitle
+        title={"Conheça Os profissionais"}
+        subtitle={
+          "Preencha seu endereço e veja todos os profissionais da sua localidade"
+        }
+      />
+      <UserInformation
+        picture={"https://github.com/ClebyFrancisco.png"}
+        name={"Cleby Francisco"}
+        rating={4}
+        description={"Pernambuco"}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
